3b2469862839917c0e57b7d64bc31ff673115f38
muheda <yunwei@muheda.com>
2018-07-31 17:46:46 +0800

    Initial Commit

---
+++ JLRouterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLRouterDemo
+//
+//  Created by 于朝盼 on 2018/7/31.
+//  Copyright © 2018年 于朝盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JLRouterDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JLRouterDemo
+//
+//  Created by 于朝盼 on 2018/7/31.
+//  Copyright © 2018年 于朝盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
